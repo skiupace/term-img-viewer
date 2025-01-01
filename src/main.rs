f687c4c858b667032a0b0aba42ce91c0d474b62a
skiupace <ahmedalhakemee200335@gmail.com>
2025-01-01 10:47:49 +0300

    did some final refactoring

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ fn main()-> Result<(), u16> {
         return Err(0);
     }
 
-    // The first argument (args[1]) is the image path
     let img_path = Path::new(&args[1]);
 
     if !img_path.exists() {
@@ -26,8 +25,6 @@ fn main()-> Result<(), u16> {
         return Err(0);
     }
 
-    // Display `img.jpg` with dimensions 80×25 in terminal cells.
-    // The image resolution will be 80×50 because each cell contains two pixels.
     print_from_file(img_path, &conf).expect("Image printing failed.");
 
     Ok(())
